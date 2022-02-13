7e7065ee4c9f86d419409430cfb6406254e1f653
Joshua Morony <joshua.morony@gmail.com>
2022-02-13 22:13:26 +1030

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'reactive-pagination',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
